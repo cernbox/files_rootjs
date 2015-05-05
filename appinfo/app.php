0c6f2b832a570773a093acdc6e7b0aa29da5ef3e
Hugo Gonzalez Labrador <hglavra@gmail.com>
2015-05-05 16:32:36 +0200

    Init Files Rootjs plugin

---
+++ appinfo/app.php
@@ -0,0 +1,21 @@
+<?php
+
+// only load root editor if the user is logged in
+if (\OCP\User::isLoggedIn()) {
+	
+	OCP\Util::addStyle('files_rootjs', 'style');
+	OCP\Util::addStyle('files_rootjs', 'JSRootPainter.min');
+	OCP\Util::addStyle('files_rootjs', 'JSRootInterface.min');
+
+	OCP\Util::addscript('files_rootjs', 'editor');
+	OCP\Util::addscript('files_rootjs', 'scripts/JSRootCore.min');
+	OCP\Util::addscript('files_rootjs', 'scripts/d3.v3.min');
+	OCP\Util::addscript('files_rootjs', 'scripts/JSRootPainter.min');
+	OCP\Util::addscript('files_rootjs', 'scripts/JSRootInterface.min');
+	OCP\Util::addscript('files_rootjs', 'scripts/JSRootPainter.jquery.min');
+	OCP\Util::addscript('files_rootjs', 'scripts/JSRootIOEvolution.min');
+	
+	/*OCP\Util::addscript('files_rootjs', 'rawinflate.min');
+	*/
+	
+}
---
+++ js/editor.js
@@ -0,0 +1,89 @@
+function showControls(dir, filename, writeable) {
+	// Loads the control bar at the top.
+	OC.Breadcrumb.show(dir, filename, '#');
+	// Load the new toolbar.
+	var editorbarhtml = '<div id="simpleGUIcontrols" style="display: none;">';
+	editorbarhtml += '<button id="simpleGUI_close" class="icon-close svg"></button>';
+	editorbarhtml += '</div>';
+
+	$('#controls').append(editorbarhtml);
+	$('#simpleGUIcontrols').show();
+	if (!OC.Util.hasSVGSupport()) {
+		OC.Util.replaceSVG($('#simpleGUIcontrols'));
+	}
+}
+
+function bindControlEvents() {
+	$('#content').on('click', '#simpleGUI_close', hideFileEditor);
+}
+
+// returns true or false if the editor is in view or not
+function editorIsShown() {
+	return is_editor_shown;
+}
+
+// Fades out the editor.
+function hideFileEditor() {
+	console.debug("closing editor");
+	$('#fileList').off('changeDirectory.texteditor');
+	if (window.FileList){
+		// reload the directory content with the updated file size + thumbnail
+		// and also the breadcrumb
+		window.FileList.reload();
+	}
+	// Fade out editor
+	$('#files_rootjs_container, #simpleGUIcontrols').remove();
+	// Reset document title
+	document.title = $('body').attr('old_title');
+	FileList.setViewerMode(false);
+	$('#content table').show();
+	is_editor_shown = false;
+}
+function textEditorOnChangeDirectory(ev){
+	// if the directory is changed, it is usually due to browser back
+	// navigation. In this case, simply close the editor
+	hideFileEditor();
+}
+
+// Loads the file editor. Accepts two parameters, dir and filename.
+function showFileEditor(dir, filename) {
+		if (!editorIsShown()) {
+			is_editor_shown = true;
+			// Delete any old editors
+			if ($('#notification').data('reopeneditor')) {
+				OC.Notification.hide();
+			}
+			$('#simpleGUI').remove();
+			// Loads the file editor and display it.
+			$('#content').append('<div id="files_rootjs_container"><div id="simpleGUI"></div></div>');
+			$('#simpleGUI').attr('files', 'https://testbox.cern.ch/index.php/apps/files/ajax/download.php?dir=' + dir + '&files=' + filename);
+			// Initialise the editor
+			if (window.FileList){
+				FileList.setViewerMode(true);
+				$('#fileList').on('changeDirectory.texteditor', textEditorOnChangeDirectory);
+			}
+			// Show the control bar
+			showControls(dir, filename, false);
+			// Update document title
+			$('body').attr('old_title', document.title);
+			document.title = filename + ' - ownCloud';
+			JSROOT.source_dir = "https://testbox.cern.ch/apps/files_rootjs/js/";
+			BuildSimpleGUI();
+		}
+}
+
+var is_editor_shown = false;
+$(document).ready(function () {
+	if ($('#isPublic').val()){
+		// disable editor in public mode (not supported yet)
+		return;
+	}
+	if (typeof FileActions !== 'undefined') {
+		FileActions.setDefault('application/x-root', 'Edit');
+                OCA.Files.fileActions.register('application/x-root', 'Edit', OC.PERMISSION_READ, '', function (filename) {
+                        showFileEditor($('#dir').val(), filename);
+                });
+		bindControlEvents();
+	}
+	
+});
