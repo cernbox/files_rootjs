92afdd352784cf0e5bad32d082320f51e96c50a0
Hugo Gonzalez Labrador <hglavra@gmail.com>
2015-06-25 11:11:24 +0200

    Enable ROOT Viewer in Public Share Mode

--- js/editor.js
+++ js/editor.js
@@ -48,84 +48,147 @@ function rootjstextEditorOnChangeDirectory(ev){
 // Loads the file editor. Accepts two parameters, dir and filename.
 function rootjsShowFileEditor(dir, filename) {
 
-		// Before trying to open the root file, we check that the size is OK until we get Byte-Range support
-		var sizeURL = OC.filePath('files_rootjs', 'ajax', 'canbeopen.php') + "?file=" + filename + "&dir=" + dir;
-                if ($('#isPublic').val()){
-	                sizeURL = window.location.pathname + '/download?path=' + dir + '&files=' + filename;
-                }
-		$.get(sizeURL)
-		.done(function(result){
-			if (result.status === 'success') {
-				if (!rootjsEditorIsShown()) {
-					is_editor_shown = true;
-					// Delete any old editors
-					if ($('#notification').data('reopeneditor')) {
-						OC.Notification.hide();
-					}
-					$('#simpleGUI').remove();
-					// Loads the file editor and display it.
-					
 
-					$('#content').append('<div id="files_rootjs_container"><div id="simpleGUI"></div></div>');
-					//var fileURL = fileDownloadPath(dir, filename);
-					var fileURL = OC.filePath('files_rootjs', 'ajax', 'loadfile.php') + "?file=" + filename + "&dir=" + dir
-					if ($('#isPublic').val()){
-						fileURL = window.location.pathname + '/download?path=' + dir + '&files=' + filename;
-					}
-					$('#simpleGUI').attr('files', fileURL);
-
-					// Initialise the editor
-					if (window.FileList){
-						FileList.setViewerMode(true);
-						$('#fileList').on('changeDirectory.texteditor', rootjstextEditorOnChangeDirectory);
-					}
-					// Show the control bar
-					rootjsShowControls(dir, filename, false);
-					// Update document title
-					$('body').attr('old_title', document.title);
-					document.title = filename + ' - ownCloud';
-					JSROOT.source_dir = window.location.origin + window.oc_webroot + "/apps/files_rootjs/js/";
-					BuildSimpleGUI();
-
-					// Adapt interface to make it simple
-					$("#simpleGUI #urlToLoad").val(fileURL);
-					$("#simpleGUI #urlToLoad").hide();
-					$("#simpleGUI select[name='s']").hide();
-					
-					var layout = $("#simpleGUI #layout");
-					var loadButton = $("#simpleGUI input[value='Load']");
-					var resetButton = $("#simpleGUI input[value='Reset']");
-					var buttonGroup = loadButton.parent();
-					var form = $("simpleGUI form");
-					var banner = "<p style='position:absolute; bottom: 0px;font-size:10px;'>This viewer is based on the <a href='http://root.cern.ch'><b>ROOT</b></a> data analysis framework developed at CERN.<br/> If you have questions or issues please refer to <a href='https://root.cern.ch/drupal/content/support'><b>ROOT Support page</b></a><br/>Integration done by <a href='https://cernbox.cern.ch'><b>CERNBOX</b></a> team</p>";
-					var left = $("#simpleGUI #left-div");
-					var right = $("#simpleGUI #right-div");
-						
-					left.append(banner);
-					loadButton.hide();
-					resetButton.hide();
-					loadButton.click();
-					layout.attr("style", "");
-					$("#simpleGUI #left-div>h1").hide();
-					$("#simpleGUI #left-div>p").hide();
-					$("#simpleGUI #left-div form>p").hide();
-					left.append(banner);
-					left.css({border:"0px", "background-color":"white"});
-					right.css({border:"0px", "background-color":"white"});
-
-					var reloadButton = $('<input type="button" id="reload" value="Reload with selected layout">');
-					buttonGroup.append(reloadButton);
-					reloadButton.on('click', function(e) {
-						resetButton.click();
-						loadButton.click()
-					});			
-
+		if($('#isPublic').val()) { // when we are in public view we cannot check the stat, and just download the file
+			 if (!rootjsEditorIsShown()) {
+				is_editor_shown = true;
+				// Delete any old editors
+				if ($('#notification').data('reopeneditor')) {
+					OC.Notification.hide();
 				}
+				$('#simpleGUI').remove();
+				// Loads the file editor and display it.
 
-			} else {
-				OC.dialogs.alert(result.data.message, t('files_texteditor', 'An error occurred!'));
+
+				$('#content').append('<div id="files_rootjs_container"><div id="simpleGUI"></div></div>');
+				var fileURL = window.location.pathname + '/download?path=' + dir + '&files=' + filename;
+				$('#simpleGUI').attr('files', fileURL);
+
+				// Initialise the editor
+				if (window.FileList){
+					FileList.setViewerMode(true);
+					$('#fileList').on('changeDirectory.texteditor', rootjstextEditorOnChangeDirectory);
+				}
+				// Show the control bar
+				rootjsShowControls(dir, filename, false);
+				// Update document title
+				$('body').attr('old_title', document.title);
+				document.title = filename + ' - ownCloud';
+				JSROOT.source_dir = window.location.origin + window.oc_webroot + "/apps/files_rootjs/js/";
+				BuildSimpleGUI();
+
+				// Adapt interface to make it simple
+				$("#simpleGUI #urlToLoad").val(fileURL);
+				$("#simpleGUI #urlToLoad").hide();
+				$("#simpleGUI select[name='s']").hide();
+
+				var layout = $("#simpleGUI #layout");
+				var loadButton = $("#simpleGUI input[value='Load']");
+				var resetButton = $("#simpleGUI input[value='Reset']");
+				var buttonGroup = loadButton.parent();
+				var form = $("simpleGUI form");
+				var banner = "<p style='position:absolute; bottom: 0px;font-size:10px;'>This viewer is based on the <a href='http://root.cern.ch'><b>ROOT</b></a> data analysis framework developed at CERN.<br/> If you have questions or issues please refer to <a href='https://root.cern.ch/drupal/content/support'><b>ROOT Support page</b></a><br/>Integration done by <a href='https://cernbox.cern.ch'><b>CERNBOX</b></a> team</p>";
+				var left = $("#simpleGUI #left-div");
+				var right = $("#simpleGUI #right-div");
+
+				left.append(banner);
+				loadButton.hide();
+				resetButton.hide();
+				loadButton.click();
+				layout.attr("style", "");
+				$("#simpleGUI #left-div>h1").hide();
+				$("#simpleGUI #left-div>p").hide();
+				$("#simpleGUI #left-div form>p").hide();
+				left.append(banner);
+				left.css({border:"0px", "background-color":"white"});
+				right.css({border:"0px", "background-color":"white"});
+
+				var reloadButton = $('<input type="button" id="reload" value="Reload with selected layout">');
+				buttonGroup.append(reloadButton);
+				reloadButton.on('click', function(e) {
+					resetButton.click();
+					loadButton.click()
+				});
+
 			}
-		})
+
+		} else {
+			// Before trying to open the root file, we check that the size is OK until we get Byte-Range support
+			var sizeURL = OC.filePath('files_rootjs', 'ajax', 'canbeopen.php') + "?file=" + filename + "&dir=" + dir;
+			$.get(sizeURL)
+			.done(function(result){
+				if (result.status === 'success') {
+					if (!rootjsEditorIsShown()) {
+						is_editor_shown = true;
+						// Delete any old editors
+						if ($('#notification').data('reopeneditor')) {
+							OC.Notification.hide();
+						}
+						$('#simpleGUI').remove();
+						// Loads the file editor and display it.
+						
+
+						$('#content').append('<div id="files_rootjs_container"><div id="simpleGUI"></div></div>');
+						//var fileURL = fileDownloadPath(dir, filename);
+						var fileURL = OC.filePath('files_rootjs', 'ajax', 'loadfile.php') + "?file=" + filename + "&dir=" + dir
+						if ($('#isPublic').val()){
+							fileURL = window.location.pathname + '/download?path=' + dir + '&files=' + filename;
+						}
+						$('#simpleGUI').attr('files', fileURL);
+
+						// Initialise the editor
+						if (window.FileList){
+							FileList.setViewerMode(true);
+							$('#fileList').on('changeDirectory.texteditor', rootjstextEditorOnChangeDirectory);
+						}
+						// Show the control bar
+						rootjsShowControls(dir, filename, false);
+						// Update document title
+						$('body').attr('old_title', document.title);
+						document.title = filename + ' - ownCloud';
+						JSROOT.source_dir = window.location.origin + window.oc_webroot + "/apps/files_rootjs/js/";
+						BuildSimpleGUI();
+
+						// Adapt interface to make it simple
+						$("#simpleGUI #urlToLoad").val(fileURL);
+						$("#simpleGUI #urlToLoad").hide();
+						$("#simpleGUI select[name='s']").hide();
+						
+						var layout = $("#simpleGUI #layout");
+						var loadButton = $("#simpleGUI input[value='Load']");
+						var resetButton = $("#simpleGUI input[value='Reset']");
+						var buttonGroup = loadButton.parent();
+						var form = $("simpleGUI form");
+						var banner = "<p style='position:absolute; bottom: 0px;font-size:10px;'>This viewer is based on the <a href='http://root.cern.ch'><b>ROOT</b></a> data analysis framework developed at CERN.<br/> If you have questions or issues please refer to <a href='https://root.cern.ch/drupal/content/support'><b>ROOT Support page</b></a><br/>Integration done by <a href='https://cernbox.cern.ch'><b>CERNBOX</b></a> team</p>";
+						var left = $("#simpleGUI #left-div");
+						var right = $("#simpleGUI #right-div");
+							
+						left.append(banner);
+						loadButton.hide();
+						resetButton.hide();
+						loadButton.click();
+						layout.attr("style", "");
+						$("#simpleGUI #left-div>h1").hide();
+						$("#simpleGUI #left-div>p").hide();
+						$("#simpleGUI #left-div form>p").hide();
+						left.append(banner);
+						left.css({border:"0px", "background-color":"white"});
+						right.css({border:"0px", "background-color":"white"});
+
+						var reloadButton = $('<input type="button" id="reload" value="Reload with selected layout">');
+						buttonGroup.append(reloadButton);
+						reloadButton.on('click', function(e) {
+							resetButton.click();
+							loadButton.click()
+						});			
+
+					}
+
+				} else {
+					OC.dialogs.alert(result.data.message, t('files_texteditor', 'An error occurred!'));
+				}
+			})
+		}
 
 
 
